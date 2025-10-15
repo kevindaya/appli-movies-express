7fa5827f14f56af54024ace6b80b03763c30aa89
Kevin Ndaya <ndayakevin2007@gmail.com>
2025-10-15 18:30:27 +0100

    Ajout des routes /movies et meilleure structuration du dossier learning-node.js

---
+++ README.md
@@ -0,0 +1,27 @@
+# Learning Backend
+
+## 🎯 Objectif
+
+Devenir Backend Developer avec Node.js + Fastify
+
+## 📅 Début
+
+14 Octobre 2025
+
+## 🛠️ Stack
+
+- Node.js
+- Express.js → Fastify
+- MYSQL
+- Redis
+- JWT
+
+## 💪 Engagement
+
+- 10-12h de code/semaine
+- Pas de jeux lundi-mardi-jeudi
+- 1 commit minimum par jour de code
+
+---
+
+"Je choisis mon futur, pas ma distraction."
--- app.js
+++ app.js
@@ -3,11 +3,23 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/", function (req, res) {
+app.get("/movies", (req, res) => {
+  res.send("Bientôt des films ici même");
+});
+
+app.get("/movies/add", (req, res) => {
+  res.send("Prochainement un formulaire d'ajout");
+});
+
+app.get("/movies/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`film numéro ${id}`);
+});
+
+app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(PORT, function () {
   console.log(`listening on port ${PORT}`);
 });
-
